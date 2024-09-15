23e01d7d983949cd36977fd36af2cc9eaeec8d84
Arnab Bir <arnabbir@gmail.com>
2024-09-15 16:16:11 +0530

    rolled back to default

--- src/pages/HappyBirthday.jsx
+++ src/pages/HappyBirthday.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { Button } from "@/components/ui/button";
+import PortfolioSummary from '../components/portfoliohub/PortfolioSummary';
+import AssetLiabilityChart from '../components/portfoliohub/AssetLiabilityChart';
+import PortfolioDistribution from '../components/portfoliohub/PortfolioDistribution';
+import FireCalculator from '../components/portfoliohub/FireCalculator';
+import IncomeExpenseChart from '../components/portfoliohub/IncomeExpenseChart';
+import NetWorthChart from '../components/portfoliohub/NetWorthChart';
+import FinancialDataForm from '../components/portfoliohub/FinancialDataForm';
+import ThemeToggle from '../components/portfoliohub/ThemeToggle';
+import { exportToExcel, importFromExcel } from '../utils/excelUtils';
+
 import TypewriterEffect from '../components/TypewriterEffect';
 
 const HappyBirthday = () => {
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-500 to-purple-500">
       <h1 className="text-6xl font-bold text-white">
